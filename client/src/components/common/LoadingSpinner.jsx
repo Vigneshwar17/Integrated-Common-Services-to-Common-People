8325ffccae60588bccef367f6be3a4eae72fe4ae
Vigneshwar S <145089209+Vigneshwar17@users.noreply.github.com>
2025-07-19 20:09:47 +0530

    Delete client directory

--- client/src/components/common/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-primary"></div>
-    </div>
-  );
-};
-
-export default LoadingSpinner;
